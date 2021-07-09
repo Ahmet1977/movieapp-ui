43db959faff727a66f4e8fde8068d6cdbfbff3b9
Ahmet1977 <carpfish1977@gmail.com>
2021-07-08 21:46:19 -0400

    Create Store, provide Store, create rootReducer/movieReducer

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     
+    App Component
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/reducers/moviesReducer.js
@@ -0,0 +1,15 @@
+// Create with rxreducer snippet
+const initialState = {
+
+}
+
+export default (state = [], { type, payload }) => {
+    switch (type) {
+
+  /*  case typeName:
+        return { ...state, ...payload } */
+
+    default:
+        return state
+    }
+}
