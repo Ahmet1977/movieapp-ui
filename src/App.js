3521500e7082323d839f515a2bcf70bbc4b983ed
Ahmet1977 <carpfish1977@gmail.com>
2021-07-10 16:06:57 -0400

    Semantic added, Header, Footer created

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
-import './App.css';
-import {Link, Route} from  'react-router-dom'
-import MoviesPage from './components/pages/MoviesPage';
+import "./App.css";
+import {Route } from "react-router-dom";
+import MoviesPage from "./components/pages/MoviesPage";
+import Footer from "./components/Footer";
+import Header from "./components/Header"
+import React, { Component } from "react";
+import "semantic-ui-css/semantic.min.css";
+import { Container} from "semantic-ui-react";
 
-function App() {
-  return (
-    <div className="App">
-     
-    App Component
-    <br/>
-    <Link to="movies">Movies</Link>
-    <Route path='/movies' component={MoviesPage}></Route>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Header/>
+        <Container text>
+          App Component
+          <br />
+          <Route path="/movies" component={MoviesPage}></Route>
+        </Container>
+        <Footer />
+      </div>
+    );
+  }
 }
-
 export default App;
--- src/components/pages/MoviesPage.js
+++ src/components/pages/MoviesPage.js
@@ -1,23 +1,29 @@
 // Created with rcredux snippet
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-
+import moviesReducer from '../../reducers/moviesReducer'
+import PropTypes from 'prop-types'
+import MovieList from '../MovieList'
 export class MoviesPage extends Component {
-    render() {
-        return (
-            <div>
-                Movies Page Component
-            </div>
-        )
-    }
+  static propTypes = {movies:PropTypes.array.isRequired}
+  render() {
+    console.log(this.props)
+    return (
+      <div>
+        Movies Page Component
+        <br/>
+        <MovieList movies={this.props.movies}/>
+        <hr />
+      </div>
+    )
+  }
 }
 
-const mapStateToProps = (state) => ({
-    
-})
+//const mapStateToProps = (state) => ({movies:state.moviesReducer})
+const mapStateToProps = ({moviesReducer}) => ({movies:moviesReducer})
 
 const mapDispatchToProps = {
-    
+  
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(MoviesPage)
