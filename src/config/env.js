be08fb15e86f8a74b4c318fd289b58da85cfc920
Ahmet1977 <carpfish1977@gmail.com>
2021-07-13 23:05:00 -0400

    Redux Promise Middleware, Loader, Link to Homepage with Navlink

---
+++ src/config/env.js
@@ -0,0 +1,1 @@
+export const API_BASE = "https://60ecdba3a78dc700178adc4a.mockapi.io"
--- src/index.js
+++ src/index.js
@@ -3,26 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { createStore, applyMiddleware } from 'redux'
+import  { createStore, applyMiddleware }  from 'redux'
 import rootReducer from './reducers/rootReducer'
 import thunk from 'redux-thunk';
 import {composeWithDevTools} from 'redux-devtools-extension'
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-const myStore = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
-
-
-
+import logger from 'redux-logger'
+import reduxPromise from 'redux-promise-middleware'
+const myStore = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk, reduxPromise, logger)))
 ReactDOM.render(
   <BrowserRouter>
     <Provider store={myStore}>
-
       <App />
-
     </Provider>
-  </BrowserRouter>
-  ,
-   
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
